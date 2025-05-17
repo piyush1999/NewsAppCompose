6599b3fae9925ebdd9ee375c577b6c92ef1430c7
piyush likhitkar <piyushlikhitkar1999@gmail.com>
2025-05-18 00:00:54 +0530

    Hilt Setup

---
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.newsapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import androidx.lifecycle.lifecycleScope
+import com.example.newsapp.domain.usecases.app_entry.AppEntryUseCases
+import com.example.newsapp.presentation.onboarding.OnBoardingScreen
+import com.example.newsapp.ui.theme.NewsAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var appEntryUseCases: AppEntryUseCases
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        installSplashScreen()
+        setContent {
+            NewsAppTheme(
+                dynamicColor = false
+            ) {
+                Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
+                    OnBoardingScreen()
+                }
+            }
+        }
+        lifecycleScope.launch {
+            appEntryUseCases.readAppEntry().collect{
+                Log.d("Test",it.toString())
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/newsapp/di/AppModule.kt
@@ -0,0 +1,33 @@
+package com.example.newsapp.di
+
+import android.app.Application
+import com.example.newsapp.data.manger.LocalUserMangerImpl
+import com.example.newsapp.domain.manger.LocalUserManger
+import com.example.newsapp.domain.usecases.app_entry.AppEntryUseCases
+import com.example.newsapp.domain.usecases.app_entry.ReadAppEntry
+import com.example.newsapp.domain.usecases.app_entry.SaveAppEntry
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideLocalUserManager(
+        application: Application
+    ):LocalUserManger= LocalUserMangerImpl(application)
+
+    @Provides
+    @Singleton
+    fun provideAppEntryUseCases(
+        localUserManger: LocalUserManger
+    ): AppEntryUseCases = AppEntryUseCases(
+        readAppEntry = ReadAppEntry(localUserManger),
+        saveAppEntry = SaveAppEntry(localUserManger)
+    )
+}
